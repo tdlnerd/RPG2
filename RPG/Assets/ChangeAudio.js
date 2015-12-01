a7af6a939b450b097ad8eaf2569278a13ed2c657
Luce Tanner <tanner.luce17@beltonnewtech.org>
2015-12-01 15:55:50 -0600

    Code Test

--- RPG/Assets/ChangeAudio.js
+++ RPG/Assets/ChangeAudio.js
@@ -6,6 +6,9 @@ Source = GameObject.Find("Main Camera");
 
 function OnTriggerEnter (hit: Collider) {
 if (hit.gameObject.tag == "OWPlayer") {
+	if (Source.GetComponent.<AudioSource>().clip == Music) {
+	return;
+	}
 Source.GetComponent.<AudioSource>().Stop();
 Source.GetComponent.<AudioSource>().clip = Music;
 Source.GetComponent.<AudioSource>().Play();
