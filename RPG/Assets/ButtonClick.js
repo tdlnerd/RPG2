cc8e5a47ccb09c8f41a43c56f07c0ec705562722
Luce Tanner <tanner.luce17@beltonnewtech.org>
2015-12-02 10:44:14 -0600

    Snake boss

---
+++ RPG/Assets/ButtonClick.js
@@ -0,0 +1,5 @@
+﻿var L2L : String;
+
+function Trans () {
+	Application.LoadLevel(L2L);
+}
---
+++ RPG/Assets/SnakeAI.js
@@ -0,0 +1,9 @@
+﻿var Anim : Animator;
+
+function Start () {
+}
+
+function Update () {
+	transform.Translate(Vector3(0, 0, Time.deltaTime));
+	Anim.SetBool("Moving", true);	
+}
