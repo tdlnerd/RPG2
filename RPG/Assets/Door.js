527a5287719000318b17b67aa106325d0d9e59f7
Luce Tanner <tanner.luce17@beltonnewtech.org>
2015-12-09 10:44:58 -0600

    Doors require keys

--- RPG/Assets/Door.js
+++ RPG/Assets/Door.js
@@ -1,6 +1,17 @@
 ﻿var Target : Vector3;
+var RequireKey = false;
+var Unlock = true;
 function OnTriggerStay2D (hit : Collider2D) {
 if (hit.gameObject.tag == "Player" && Input.GetKeyDown("up")) {
+	if (RequireKey == true && Unlock == false) {
+	var Keys = hit.GetComponent.<ItemUse>().KeyCount;
+		if (Keys > 0) {
+		hit.GetComponent.<ItemUse>().KeyCount -= 1;
+		Unlock = true;
+	}
+	}
+if (Unlock == true) {
 hit.transform.position = Target;
 }
-}  
+}
+} 
--- RPG/Assets/ItemUse.js
+++ RPG/Assets/ItemUse.js
@@ -1,8 +1,10 @@
 ﻿var BombMenu : UI.Toggle;
 var SwordMenu : UI.Toggle;
 var BombCounter : UI.Text;
+var KeyCounter : UI.Text;
 var Bomb : GameObject;
 var BombCount : int;
+var KeyCount : int;
 var Anim : Animator;
 var Sword : Collider2D;
 function Update () {
@@ -19,6 +21,7 @@ function Update () {
 	BombCount = 15;
 	}
 	BombCounter.text = "x " + BombCount.ToString();
+	KeyCounter.text = "x " + KeyCount.ToString();
 }
 
 function Attack() {
@@ -27,4 +30,11 @@ Sword.enabled = true;
 yield WaitForSeconds(0.5);
 Anim.SetBool("Sword", false);
 Sword.enabled = false;
+}
+
+function OnCollisionEnter2D (hit : Collision2D) {
+	if (hit.gameObject.tag == "GenKey") {
+	KeyCount += 1;
+	Destroy(hit.gameObject);
+	}
 }
