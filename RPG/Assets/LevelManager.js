59f126af289181f2d98ac3c798a54e4626289094
Luce Tanner <tanner.luce17@beltonnewtech.org>
2016-01-04 10:46:35 -0600

    Improvements

--- RPG/Assets/LevelManager.js
+++ RPG/Assets/LevelManager.js
@@ -3,11 +3,18 @@ var PlayerSprite : GameObject;
 var Lighting : Color32;
 var Cam : GameObject;
 var Music : AudioClip;
+var DisCamera = false;
+var RequireDefeat = false;
+var Exit : GameObject;
+
 function Start () {
 yield WaitForSeconds (0.1);
 PlayerSprite.GetComponent(Renderer).material.color = Lighting;
 Cam.GetComponent.<AudioSource>().clip = Music;
 Cam.GetComponent.<AudioSource>().Play();
+	if (DisCamera == true) {
+	Cam.GetComponent.<Camera>().enabled = false;
+	}
 }
 
 function Update () {
@@ -20,4 +27,7 @@ PlayerSprite = GameObject.Find("Knight Walking_0");
 if (Knight == null) {
 Knight = GameObject.Find("Knight");
 }
+	if (RequireDefeat == true && GameObject.FindGameObjectsWithTag("Enemy").Length < 1) {
+	Exit.SetActive(true);
+	}
 }
