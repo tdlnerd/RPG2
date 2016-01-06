506cba0128629c3ff45e5ce2f3a7c4a5d85618f7
Tanner <tanner.luce17@beltonnewtech.org>
2016-01-06 09:54:11 -0600

    Functional NPC

--- RPG/Assets/NPCAI.js
+++ RPG/Assets/NPCAI.js
@@ -2,6 +2,22 @@
 var Choose = false;
 var Anim : Animator;
 var speed : float;
+var Timed = true;
+var StartH = System.DateTime.Now.Hour;
+var EndH = System.DateTime.Now.Hour;
+var Hour = System.DateTime.Now.Hour;
+var corrected = false;
+
+function Start () {
+Hour = System.DateTime.Now.Hour;
+	if (Hour > StartH && Hour < EndH) {
+	gameObject.SetActive(true);
+	}
+	else {
+	gameObject.SetActive(false);
+	}
+}
+
 function Update () {
 if (Choose == false) {
 Activity();
@@ -9,11 +25,17 @@ Activity();
 if (Walking == true) {
  transform.Translate(Vector2((0.1 * speed * Time.deltaTime),0));
  }
-if (speed > 0) {
+ if (Walking == true && speed == 0) {
+ Idle();
+ }
+ Debug.Log(speed);
+ if (speed > 0) {
 	transform.localScale.x = Mathf.Abs(transform.localScale.x);
+	corrected = false;
 	}
-	if (speed < 0) {
+if (speed < 0 && corrected == false) {
 	transform.localScale.x *= -1;
+	corrected = true;
 	}
 }
 
@@ -30,9 +52,8 @@ var choice = Random.Range(0,2);
 }
 
 function Walk () {
-	if (Walking == false) {
+Choose = true;
 speed = Random.Range(-3,3);
-}
 Walking = true;
 Anim.SetBool("WalkR", true);
 Anim.SetFloat("Speed", (Mathf.Abs(speed/3)));
@@ -41,6 +62,7 @@ Choose = false;
 }
 
 function Idle () {
+Choose = true;
 Anim.SetBool("WalkR", false);
 Walking = false;
 yield WaitForSeconds (Random.Range(5,10));
