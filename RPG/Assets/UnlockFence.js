6be5109c983a3ddf0bfd729da4b44fd34d939577
Luce Tanner <tanner.luce17@beltonnewtech.org>
2016-01-07 10:46:20 -0600

    Changes

--- RPG/Assets/UnlockFence.js
+++ RPG/Assets/UnlockFence.js
@@ -3,20 +3,24 @@ var Unlocked : String;
 var Text : String;
 var MessageBox : UI.Text;
 var Message : String;
+var Fence : GameObject;
+var Bool : int;
+var UnlockedBool : int;
 function Start () {
 	MessageBox = GameObject.Find("MainTextBox").GetComponent(UI.Text);
-	PlayerPrefs.GetInt(Check);
-	if (Check == 1 && Unlocked == 1 ) {
-	gameObject.SetActive(false);
+	Bool = PlayerPrefs.GetInt(Check);
+	UnlockedBool = PlayerPrefs.GetInt(Unlocked);
+	if (Bool == 1 && UnlockedBool == 1 ) {
+	Fence.SetActive(false);
 	}
 }
 
 function OnCollisionEnter2D (hit : Collision2D) {
-	if (hit.gameObject.tag == "Player" && Check == 1) {
+	if (hit.gameObject.tag == "Player" && Bool == 1) {
 	PlayerPrefs.SetInt(Unlocked, 1);
 	MessageBox.text = Message;
 	yield WaitForSeconds (1);
 	MessageBox.text = " ";
-	gameObject.SetActive(false);
+	Fence.SetActive(false);
 	}
 }
