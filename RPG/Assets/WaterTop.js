02a15fd500031f5b68e03bf6520d1a650f02da3e
Tanner Luce <tdlnerd@gmail.com>
2016-01-20 19:48:53 -0600

    Functional boat programming

---
+++ RPG/Assets/WaterTop.js
@@ -0,0 +1,25 @@
+﻿var Col : Collider;
+var Anim : Animator; 
+function Start () {
+	if (PlayerPrefs.GetInt("Boat") == 1) {
+	Col.enabled = false;
+	}
+}
+
+function Update () {
+	if (Anim == null) {
+	Anim = GameObject.Find("Map View3").GetComponent(Animator);
+	}
+}
+
+function OnTriggerStay (hit : Collider) {
+	if (hit.gameObject.tag == "OWPlayer" && PlayerPrefs.GetInt("Boat") == 1) {
+	Anim.SetBool("Boat", true);
+	}
+}
+
+function OnTriggerExit (hit : Collider) {
+	if (hit.gameObject.tag == "OWPlayer") {
+	Anim.SetBool("Boat", false);
+	}
+}
