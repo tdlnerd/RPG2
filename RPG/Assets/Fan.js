01af732c8bf3f1d2d33e705c55b62bad8b849008
Luce Tanner <tanner.luce17@beltonnewtech.org>
2016-02-03 10:41:40 -0600

    Rooms

--- RPG/Assets/Fan.js
+++ RPG/Assets/Fan.js
@@ -1,14 +1,21 @@
 ﻿var Force : int;
 var Water = false;
 var PS : EllipsoidParticleEmitter;
+var Vert = false;
+var Hor = true;
 function OnTriggerEnter2D (hit: Collider2D) {
 	if (hit.gameObject.tag == "Player" && Water == true) {
+		if (Hor == true) {
 	hit.GetComponent.<Rigidbody2D>().AddForce(Vector2(Force,0));
-	hit.GetComponent.<Rigidbody2D>().AddTorque(Force);
+	}
+	if (Vert == true) {
+	hit.GetComponent.<Rigidbody2D>().AddForce(Vector2(0,Force));
+	}
 	}
 	if (hit.gameObject.tag == "Water") {
 	Water = true;
 	PS.emit = true;
+	GetComponent.<Animator>().SetBool("Turn", true);
 	}
 }
 
@@ -16,5 +23,6 @@ function OnTriggerEnter2D (hit: Collider2D) {
 	if (hit.gameObject.tag == "Water") {
 	Water = false;
 	PS.emit = false;
+	GetComponent.<Animator>().SetBool("Turn", false);
 	}
 	}
