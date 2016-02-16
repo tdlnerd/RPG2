6b46bd8ff87f6e94029aad36950445f6f555f6d0
Luce Tanner <tanner.luce17@beltonnewtech.org>
2016-02-16 10:44:44 -0600

    Potion

---
+++ RPG/Assets/Potion.js
@@ -0,0 +1,10 @@
+﻿var damage : UI.Slider;
+
+function Start () {
+damage = GameObject.Find("Slider").GetComponent(UI.Slider);
+}
+function Restore () {
+var Health = damage.value / 0.25;
+damage.value += Health;
+Destroy(gameObject);
+}
