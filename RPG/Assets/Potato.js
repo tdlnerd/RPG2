65e42d73cd5b38b9afee39fb5758b2d2a3e9a086
Luce Tanner <tanner.luce17@beltonnewtech.org>
2016-02-22 15:51:38 -0600

    Potato

---
+++ RPG/Assets/Potato.js
@@ -0,0 +1,11 @@
+﻿var Knight : Damage2D;
+function Start () {
+Knight = GameObject.Find("Knight").GetComponent(Damage2D);
+Knight.enabled = false;
+yield WaitForSeconds (Random.Range(10,20));
+Knight.enabled = true;
+}
+
+function Update () {
+
+}
