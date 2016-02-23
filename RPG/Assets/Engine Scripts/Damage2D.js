5228c73c45f7263814c440a4fe6166d66fa56007
Luce Tanner <tanner.luce17@beltonnewtech.org>
2016-02-23 16:33:52 -0600

    Lift Gears

--- RPG/Assets/Engine Scripts/Damage2D.js
+++ RPG/Assets/Engine Scripts/Damage2D.js
@@ -1,12 +1,13 @@
 ﻿var damage : UI.Slider;
+var Mortal = true;
 function OnCollisionEnter2D (hit: Collision2D) {
-	if (hit.gameObject.tag == "Enemy") {
+	if (hit.gameObject.tag == "Enemy" && Mortal == true) {
 	damage.value -= hit.gameObject.GetComponent(EnemyDamage).Dam;
 }
 }
 
 function OnTriggerEnter2D (hit: Collider2D) {
-	if (hit.gameObject.tag == "Enemy") {
+	if (hit.gameObject.tag == "Enemy" && Mortal == true) {
 	damage.value -= hit.gameObject.GetComponent(EnemyDamage).Dam;
 }
 }
